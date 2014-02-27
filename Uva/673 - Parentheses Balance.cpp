666e61033a33aff444e2124b3cc6930d69e08498
Caynan <caynanvls@gmail.com>
2014-02-26 22:43:43 -0300

    673 - Parentheses Balance

---
+++ Uva/673 - Parentheses Balance.cpp
@@ -0,0 +1,50 @@
+# include <iostream>
+# include <cstdio>
+# include <stack>
+
+using namespace std;
+
+
+int main(){
+	freopen ("in.txt", "r", stdin);
+	freopen ("out.txt", "w", stdout);
+
+	int n;
+	cin >> n; cin.ignore();
+	while (n--){
+		stack<char> s;
+		string word;
+		getline(cin, word);
+
+		// validity of word logic
+		for (int i = 0; i < word.length(); i++){
+			if(word[i] == ' '); // ignore empty spaces
+			else if (word[i] == '(' || word[i] == '['){
+				s.push(word[i]);
+			}
+			else if (word[i] == ')'){
+				if(s.empty() || s.top() != '('){
+					s.push('$');
+					break;
+				}else{
+					s.pop();
+				}
+			}
+			else if (word[i] == ']'){
+				if(s.empty() || s.top() != '['){
+					s.push('$');
+					break;
+				}else{
+					s.pop();
+				}
+			}
+		}
+
+		// print logic
+		if(s.empty()){ 
+			cout << "Yes\n";
+		}else{
+			cout << "No\n";
+		}
+	}
+}
