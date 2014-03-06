0d2d310ebcd50ff56e90d62307b82a02f1868940
Caynan Landim <caynanvls@gmail.com>
2014-03-06 18:17:20 -0300

    Uva 10346 - Peter Smokes in CPP

---
+++ Uva/10346 - Peter's Smokes.cpp
@@ -0,0 +1,68 @@
+/*
+user: caynanvls (http://uhunt.felix-halim.net/id/183008)
+
+problem: 
+link: 
+*/
+ 
+# include <iostream>
+# include <cstdio>
+# include <cstring>
+# include <cstdlib>
+# include <cmath>
+ 
+# include <algorithm>
+# include <vector>
+# include <queue>
+# include <stack>
+# include <map>
+# include <utility>
+# include <functional>
+ 
+ 
+#define FOR(i, s, e) for(int i=s; i<e; i++)
+#define loop(i, n) FOR(i, 0, n)
+#define tr(cont, it) for(typeof(cont.begin()) it=cont.begin() ; it != cont.end() ; it++)
+#define gi(a) sf("%d", &a)
+#define gi2(a, b) sf("%d%d", &a, &b)
+#define gi3(a, b, c) sf("%d%d%d", &a, &b, &c)
+#define gi4(a, b, c, d) sf("%d%d%d%d", &a, &b, &c, &d)
+#define sf scanf
+#define pf printf
+#define pb push_back
+#define MP make_pair
+#define fr first
+#define sc second
+#define ll long long
+#define dd double
+#define all(v) v.begin(), v.end()
+#define PI acos(-1.0)
+#define mem(ara, value) memset(ara, value, sizeof(ara))
+#define paii pair<int, int>
+#define pall pair<ll, ll>
+#define SZ(a) int(a.size())
+#define read freopen("input.txt", "r", stdin)
+#define write freopen("output.txt", "w", stdout)
+ 
+using namespace std;
+
+int k;
+int recursiveCount(int cigarrets, int butts) {
+	if (cigarrets == 0) return 0;
+	else {
+		int qtyCigarretes = (cigarrets + butts) / k;
+		int leftovers = (cigarrets + butts) % k;
+		return cigarrets + recursiveCount(qtyCigarretes, leftovers);
+	}
+		
+}
+
+int main(){
+//	read;
+//	write;
+	int n;
+	while (gi2(n, k) == 2){
+		int max = recursiveCount(n, 0);  // initially we don't have leftovers
+		pf("%d\n", max);
+	}
+}
