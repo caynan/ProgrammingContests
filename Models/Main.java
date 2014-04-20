9bbb209913310d3cfc72adb6b5ef0fbac2fc5442
Caynan Landim <caynanvls@gmail.com>
2014-04-20 01:05:27 -0300

    Added Java model to use on solving problems. Heavily based on Egor and Petr submissions on CodeForces.

---
+++ Models/Main.java
@@ -0,0 +1,57 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * @author Caynan Landim (caynanvls@gmail.com)
+ */
+
+
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		Task solver = new Task();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class Task {
+    public void solve(int testNumber, InputReader in, PrintWriter out) {
+        // SOLUTION GOES HERE
+    }
+}
+
+class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+        reader = new BufferedReader(new InputStreamReader(stream), 32768);
+        tokenizer = null;
+    }
+
+    public String next() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+}
