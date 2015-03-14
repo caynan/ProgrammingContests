85d43fd09e8ff95316c335589af3ad15c22050c0
Caynan <caynanvls@gmail.com>
2015-03-14 03:01:09 -0400

    added count-neighbours

--- checkio/count-neighbours.py
+++ checkio/count-neighbours.py
@@ -1,5 +1,23 @@
+from itertools import product
+
+
+def neighbours(i, j, grid):
+    neighbours = list(product(range(i-1, i+2), range(j-1, j+2)))
+    neighbours.remove((i,j))
+    l_i = len(grid)
+    l_j = len(grid[0])
+    neighbours = [(x, y) for (x, y) in neighbours if 0 <= x < l_i and
+                  0 <= y < l_j]
+
+    return neighbours
+
+
 def count_neighbours(grid, row, col):
-    nw = 
+    sum = 0
+    for i, j in neighbours(row, col, grid):
+        sum += grid[i][j]
+
+    return sum
 
 
 if __name__ == '__main__':
